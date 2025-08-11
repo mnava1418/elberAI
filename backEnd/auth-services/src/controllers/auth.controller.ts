66f9dbb4e3a69ef6d6bd87a0841abd9e9e8e48ae
mnava1418 <martin@mnavapena.com>
2025-08-11 19:51:15 -0400

    ADD: call request access endpoint

--- backEnd/auth-services/src/controllers/auth.controller.ts
+++ backEnd/auth-services/src/controllers/auth.controller.ts
@@ -5,7 +5,7 @@ export const requestAccess = async (req: Request, res: Response) => {
     try {
         const {email} = req.body
         await authService.requestAccess(email);
-        res.status(201).json({ message: 'Se pidio acceso para' + email });
+        res.status(200).json({ message: 'Hemos recibido tu solicitud. Revisa tu correo electrónico para más instrucciones.' });
     } catch (error: any) {
         res.status(500).json({ error: error.message });
     }

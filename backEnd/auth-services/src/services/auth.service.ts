e4a6800032fea6caca496207aca3b641d7587e69
mnava1418 <martin@mnavapena.com>
2025-08-17 16:31:33 -0400

    UPDATE: generate code with 6 digits

--- backEnd/auth-services/src/services/auth.service.ts
+++ backEnd/auth-services/src/services/auth.service.ts
@@ -86,5 +86,5 @@ const sendAccessResponseEmail = (email: string, isApproved: boolean, accessCode:
 };
 
 const generateAccessCode = () => {
-    return Math.floor(1000 + Math.random() * 9000);
+    return Math.floor(100000 + Math.random() * 900000);
 };

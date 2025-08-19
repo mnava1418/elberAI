4847dac4a57ee0f1c7021031a9f8de022774a12d
mnava1418 <martin@mnavapena.com>
2025-08-18 20:58:45 -0400

    ADD: request name when signup

---
+++ Elber/src/view/screens/auth/SignUpPasswordScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainView from '../../components/ui/MainView'
+
+const SignUpPasswordScreen = () => {
+    return (
+        <MainView></MainView>
+    )
+}
+
+export default SignUpPasswordScreen

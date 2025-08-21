d9f125a6972106d2e2800b197691be5a18877606
mnava1418 <martin@mnavapena.com>
2025-08-20 21:03:19 -0400

    UPDATE: check request status

--- backEnd/auth-services/src/controllers/auth.controller.ts
+++ backEnd/auth-services/src/controllers/auth.controller.ts
@@ -5,8 +5,8 @@ import { AuthRequest } from '../models/http:model';
 export const requestAccess = async (req: Request, res: Response) => {
     try {
         const {email} = req.body
-        await authService.requestAccess(email);
-        res.status(200).json({ message: 'Hemos recibido tu solicitud. Revisa tu correo electrónico para más instrucciones.' });
+        const {message} = await authService.requestAccess(email);
+        res.status(200).json({ message });
     } catch (error: any) {
         res.status(500).json({ error: error.message });
     }

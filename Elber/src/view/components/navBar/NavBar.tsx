b25975f2610af666354f5df87573d8b6c700af8f
mnava1418 <martin@mnavapena.com>
2025-12-25 21:13:34 -0500

    UPDATE: improve NavBar styles

--- Elber/src/view/components/navBar/NavBar.tsx
+++ Elber/src/view/components/navBar/NavBar.tsx
@@ -8,17 +8,21 @@ import navBarStyles from '../../../styles/navBar'
 interface NavBarProps  {
     title: string,
     leftAction?: () => void | undefined
+    leftIcon?: string
 }
 
-const NavBar = ({title, leftAction = undefined}: NavBarProps) => {
+const NavBar = ({title, leftAction = undefined, leftIcon = 'chevron-back'}: NavBarProps) => {
     const {top} = useSafeAreaInsets()
     
     return (
         <View style={[navBarStyles.container, { marginTop: top}]}>
-            {leftAction ? <NavBtn icon='chevron-back' onPress={leftAction} /> : <></>}
+            <View style={{width: 32, height: 32}}>
+                {leftAction ? <NavBtn icon={leftIcon} onPress={leftAction} /> : <></>}
+            </View>
             <View style={navBarStyles.title}>
                 <CustomText style={{fontWeight: '600', fontSize: 20}} type='text' text={title} />
             </View>
+            <View style={{width: 32, height: 32}} />
         </View>
     )
 }

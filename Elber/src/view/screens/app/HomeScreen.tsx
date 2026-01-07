acc7b0b49ac8de6dad51d5e5b649698ca6b63c49
mnava1418 <martin@mnavapena.com>
2026-01-06 22:09:57 -0500

    UPDATE: connect socket only once

--- Elber/src/view/screens/app/HomeScreen.tsx
+++ Elber/src/view/screens/app/HomeScreen.tsx
@@ -1,19 +1,7 @@
-import React, { useContext, useEffect } from 'react'
+import React from 'react'
 import MainView from '../../components/ui/MainView'
-import SocketModel from '../../../models/Socket.model'
-import { GlobalContext } from '../../../store/GlobalProvider';
 
 const HomeScreen = () => {
-  const { dispatch } = useContext(GlobalContext);
-
-    useEffect(() => {
-      SocketModel.getInstance().connect(dispatch)
-    
-      return () => {
-        SocketModel.getInstance().disconnect()
-      }
-    }, [])
-    
     return (
         <MainView navBarTitle='Elber' />
     )

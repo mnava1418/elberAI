50ee269c278c78ca600de88bfa91c585e9487c2c
mnava1418 <martin@mnavapena.com>
2026-01-10 23:18:11 -0500

    ADD: endpoint to send request access emails

--- backEnd/notification-services/src/routes/email.routes.ts
+++ backEnd/notification-services/src/routes/email.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import * as emailController from "../controllers/email.controller";
 
 const router = Router()
+
+router.post('/request', emailController.requestAccess )
 
 export default router

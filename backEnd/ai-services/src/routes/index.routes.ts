cd7eb0de23040a300e7a19d9720055de19057c95
mnava1418 <martin@mnavapena.com>
2026-02-12 19:28:28 -0500

    ADD: endpoint to delete user profile

--- backEnd/ai-services/src/routes/index.routes.ts
+++ backEnd/ai-services/src/routes/index.routes.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 import chatRoutes from './chat.routes'
+import userRoutes from './user.routes'
 
 const router = Router();
 
@@ -8,5 +9,6 @@ router.get('/', (req: Request, res: Response) => {
 });
 
 router.use('/chat', chatRoutes)
+router.use('/user', userRoutes)
 
 export default router

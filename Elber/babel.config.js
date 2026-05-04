290d35bd46ed6140d480c768e51c0e71cd05a763
mnava1418 <martin@mnavapena.com>
2026-05-04 17:56:57 -0600

    UPDATE: fix tests

--- Elber/babel.config.js
+++ Elber/babel.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-      }      
-    ],
+    ...(process.env.NODE_ENV !== 'test' ? [
+      ['module:react-native-dotenv', { moduleName: '@env', path: '.env' }],
+    ] : []),
     'react-native-worklets/plugin',
   ],
 };

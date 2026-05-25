1a94750c73b7452f796f041fb51a5f0bbbe8e919
mnava1418 <martin@mnavapena.com>
2026-05-25 18:47:26 -0400

    ADD: create target for watch

---
+++ Elber/ios/elberWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  elberWatch Watch App
+//
+//  Created by Martin Nava on 25/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elber/ios/elberWatch Watch App/elberWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  elberWatchApp.swift
+//  elberWatch Watch App
+//
+//  Created by Martin Nava on 25/05/26.
+//
+
+import SwiftUI
+
+@main
+struct elberWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

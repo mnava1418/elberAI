58e46d61c0cee90b03742296bb7fcde899b43b1e
mnava1418 <martin@mnavapena.com>
2026-05-25 17:11:32 -0400

    UPDATE: upgrade app version

--- Elber/src/view/screens/settings/SettingsScreen.tsx
+++ Elber/src/view/screens/settings/SettingsScreen.tsx
@@ -114,7 +114,7 @@ const SettingsScreen = ({navigation}: SettingsProps) => {
                         'information-circle-outline',
                         'Acerca de',
                         'Información de la aplicación',
-                        () => handleAlert('Continuar', 'Acerca de Elber', 'Elber v5.0.0'),
+                        () => handleAlert('Continuar', 'Acerca de Elber', 'Elber v5.0.1'),
                         undefined,
                         true
                     )}
